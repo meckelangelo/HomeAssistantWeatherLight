dfeacf7913998542e57cdd1d27f62dc76288803d
meckelangelo <meckelangelo@gmail.com>
2023-01-07 11:00:58 -0500

    Update weather_light.py

--- weather_light.py
+++ weather_light.py
@@ -1,8 +1,3 @@
-from datetime import datetime
-import requests
-import json
-import homeassistant.helpers.service as service
-
 # Color constants
 BLUE = [68, 100]
 AQUA = [56, 100]
@@ -15,22 +10,14 @@ RED = [0, 100]
 PURPLE = [78, 100]
 MAGENTA = [88, 100]
 
-def get_weather_data():
-    # API key and base URL
-    API_KEY = "YOUR_API_KEY"
-    latitude = 40.20013016138404
-    longitude = -76.63915972486825
+# Get the entity ID for the bulb we want to use for the weather light
+entity_id = data.get("entity_id")
 
-    # Query current weather and forecast for the next 8 hours
-    query_url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=daily,minutely&units=imperial&appid=" + API_KEY
+if entity_id is not None:
+    while True:
+        update_weather_light(entity_id)
 
-    # Make API call and store the response
-    response = requests.get(query_url)
-
-    # Convert the response to JSON
-    return response.json()
-
-def get_condition_colors(weather_data):
+def get_condition_colors(weather_sensor):
     # Initialize all the booleans to false
     will_rain = False
     will_snow = False
@@ -41,26 +28,29 @@ def get_condition_colors(weather_data):
     is_windy = False
     is_humid = False
     is_clear = False
-    is_alert = False
+    has_alert = False
 
     # Initialize an empty list of colors
     colors = []
     
-    # Initialize an empty list of conditions
-    conditions_set = []
-    
-    # Add the current conditions to the list
-    conditions_set.append(weather_data['current'])
-    
-    # Loop through the first 8 hours of forecast data and add it to the conditions
-    for hour in weather_data['hourly'][:8]:
-        conditions_set.append(hour)
+    # Create the list to store the weather conditions
+    weather_conditions = []
+
+    # Get the current and forecast weather data
+    current_weather = weather_sensor.attributes.get("current")
+    forecast_data = weather_sensor.attributes.get("forecast")[:8]
+    if current_weather and forecast_data:
+        # Combine the current and forecast weather conditions into a single list
+        weather_conditions = current_weather.get("weather") + [forecast_weather for forecast in forecast_data for forecast_weather in forecast.get("weather")]
+        # Check if there are any alerts in the current or forecast weather data
+        has_alert = any(data.get("alert") for data in ([current_weather] + forecast_data))
     
     # Loop through each condition
-    for conditions in conditions_set
-        # Get the weather id
-        weather_id = conditions['weather'][0]['id']
+    for condition in weather_conditions
 
+        # Get the weather condition ID
+        weather_id = condition.get("id")
+    
         # Check if it will rain or snow
         if 200 <= weather_id <= 599 or weather_id == 701:
             will_rain = True
@@ -72,25 +62,23 @@ def get_condition_colors(weather_data):
             will_rain = will_snow = True
 
         # Check if it's cold or hot
-        if conditions['temp'] <= 32 or conditions['feels_like'] <= 32:
+        temperature = condition.get("temperature")
+        feels_like = condition.get("feels_like")
+        if temperature <= 32 or feels_like <= 32:
             is_cold = True
-        if conditions['temp'] >= 80 or conditions['feels_like'] >= 80:
+        if temperature >= 80 or feels_like >= 80:
             is_hot = True
 
         # Check if it's cloudy, windy, or humid
-        if conditions['clouds'] > 50 and not will_rain and not will_snow and not will_sleet:
+        if condition.get("clouds") > 50 and not will_rain and not will_snow and not will_sleet:
             is_cloudy = True
-        if conditions['dew_point'] >= 65:
+        if condition.get("dew_point") >= 65:
             is_humid = True
-        if conditions['wind_speed'] >= 25 or conditions['wind_gust'] >= 30:
+        if condition.get("wind_speed") >= 25 or condition.get("wind_gust") >= 30:
             is_windy = True
 
-    # Check if there are any alerts
-    if 'alerts' in weather_data:
-        is_alert = True
-
     # If none of the above conditions are met, it's clear
-    if not (will_rain or will_snow or will_sleet or is_cold or is_hot or is_cloudy or is_windy or is_humid or is_alert):
+    if not (will_rain or will_snow or will_sleet or is_cold or is_hot or is_cloudy or is_windy or is_humid or has_alert):
         is_clear = True
 
     # Determine the colors based on the active booleans
@@ -112,31 +100,23 @@ def get_condition_colors(weather_data):
         colors.append(GREEN)
     if is_clear:
         colors.append(LIME)
-    if is_alert:
+    if has_alert:
         colors.append(RED)
 
     return colors
 
-def update_weather_light(refresh_time):
-    weather_data = get_weather_data()
+def update_weather_light(light_id):
+    weather_data = hass.states.get("weather.openweathermap_overview")
     colors = get_condition_colors(weather_data)
 
     # Change the bulb color every 5 seconds
-    while (datetime.now() < refresh_time):
-        for color in colors:
-                # Set the bulb color to the current color
-                set_bulb_color(color)
-                # Wait for 5 seconds
-                time.sleep(5)
+    for color in colors:
+        # Set the bulb color to the current color
+        set_bulb_color(light_id, color)
+        # Wait for 5 seconds
+        time.sleep(5)
 
-def set_bulb_color(color):
+def set_bulb_color(light_id, color):
     hue, saturation = color
-    # Replace YOUR_LIGHT_ENTITY_ID with the entity ID of your light bulb
-    service.call("light", "turn_on", {"entity_id": YOUR_LIGHT_ENTITY_ID, "hue": hue, "saturation": saturation})
-
-# Query the weather data and update the bulb color every 5 minutes
-while True:
-    refresh_seconds = 300
-    refresh_time = datetime.now() + datetime.timedelta(seconds=refresh_seconds)
-    
-    update_weather_light(refresh_time)
+    # Set the hue and saturation of the specified light bulb
+    service.call("light", "turn_on", {"entity_id": light_id, "hue": hue, "saturation": saturation})
